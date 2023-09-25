53f04d03db16bf95be4a0738296caa2ebf9d125c
Preston Goforth <p@osano.com>
2023-09-25 17:44:01 -0400

    Add Swift Package Manager file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ConsentSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ConsentSDK",
+            targets: ["ConsentSDK"])
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "ConsentSDK",
+            url: "https://libraries.osano.com/ios/OsanoConsentManagerSDK/OsanoConsentManagerSDK-3.0.0.zip",
+            checksum: "8361442a998fd4376b3b7ea026631b6d3a71383a83ac6c87b6038b950cb68923"
+        ),
+    ]
+)
