1ba9e1c1222b55ae7c4770920edace319d26c9cb
Ethan Fann <6005240+ethanfann@users.noreply.github.com>
2024-03-26 11:03:12 -0700

    NOTICKET Add version 3.2.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConsentSDK",
-            url: "https://libraries.osano.com/ios/OsanoConsentManagerSDK/OsanoConsentManagerSDK-3.1.1.zip",
-            checksum: "497e249dce60866f49826c96448dc612f49e7a1b446eaab5c078dedfc53eb0d5"
+            url: "https://libraries.osano.com/ios/OsanoConsentManagerSDK/OsanoConsentManagerSDK-3.2.0.zip",
+            checksum: "520d12e6ecf950a1acdc02841a72722283e79c684db2db1471e95eb097de8975"
         ),
     ]
 )
