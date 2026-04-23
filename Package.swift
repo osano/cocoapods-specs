ff92730ffada4c55460791b9780e97e8be28813a
Preston Goforth <p@osano.com>
2026-04-23 14:29:50 -0400

    [SDK-453] - Update SDK to v3.6.7

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConsentSDK",
-            url: "https://libraries.osano.com/ios/OsanoConsentManagerSDK/OsanoConsentManagerSDK-3.6.6.zip",
-            checksum: "8b67804b7f2c62f1d60f69b2350105473d33c708a172e375d408521d06d1b3a3"
+            url: "https://libraries.osano.com/ios/OsanoConsentManagerSDK/OsanoConsentManagerSDK-3.6.7.zip",
+            checksum: "63172524d4b9be1cb371f20bdff19bf09de2081ee9a6385a386d56f6ffa1e80c"
         ),
     ]
 )
